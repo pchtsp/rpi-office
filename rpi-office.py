fac26ea5aa42201899f92cdef762903270691059
Franco <pchtsp@gmail.com>
2016-03-04 17:28:19 +0000

    despliegue

--- rpi-office.py
+++ rpi-office.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, jsonify
 import time
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 num_pin = 17
+num_pin2= 25
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(num_pin, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(num_pin, GPIO.OUT)
+GPIO.setup(num_pin2, GPIO.OUT)
+GPIO.output(num_pin2, True)
+
 app = Flask(__name__)
 app.debug = True
 
@@ -15,15 +19,16 @@ def hello_world():
 
 @app.route('/turn_light_on')
 def turn_light_on():
-    # GPIO.output(num_pin, True)
+    GPIO.output(num_pin, True)
     data = "prueba de encendido"
     return jsonify(result=data)
 
 @app.route('/turn_light_off')
 def turn_light_off():
-    # GPIO.output(num_pin, False)
+    GPIO.output(num_pin, False)
     data = "prueba de apagado"
     return jsonify(result=data)
 
 if __name__ == '__main__':
-    app.run()
+#    app.run()
+    app.run(host='0.0.0.0')
